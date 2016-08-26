5bd56883012f132d321d252296922c99e85b8e3b
Tarun Kothuri <tkothuri@appnexus.com>
2016-08-26 13:19:45 -0400

    soft reset

--- pull.sh
+++ pull.sh
@@ -4,6 +4,6 @@ keyboard_dir='mnt-kinesis-keyboard'
 config_dir='kinesis-config'
 
 #rsync -avzL $keyboard_dir $config_dir
-cp -r $keyboard_dir $config_dir
+cp -r $keyboard_dir/* $config_dir/
 
 ls -l $config_dir
